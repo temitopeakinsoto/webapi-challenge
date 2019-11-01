b53ef9b7cad7f143e2b74b220695b72e2a3019d3
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-01 12:13:15 +0100

    Create a get endpoint for a specified ACTION resource
    In this commit, I created a new endpoint for retrieving a specified action by Id. This endpoint makes use of a validateActionId middleware to check if the specified action exists or not

--- data/Routers/actionsRouter.js
+++ data/Routers/actionsRouter.js
@@ -3,6 +3,25 @@ const actionsDb = require("../helpers/actionModel");
 const projectsDb = require("../helpers/projectModel");
 const router = express.Router();
 
+function validateActionId(req, res, next){
+    const { id } =  req.params;
+    actionsDb
+    .get(id)
+    .then(action => {
+      if (action) {
+        next();
+      } else {
+        res.status(400).json({ message: "There is no Action with the specified id" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: `Something terrible happend while checking user id: ${error.message}`
+      });
+    });
+}
+
+
 router.get("/", (req, res) => {
   actionsDb
     .get()
@@ -15,5 +34,19 @@ router.get("/", (req, res) => {
       });
     });
 });
+
+router.get("/:id", validateActionId, (req, res) => {
+    const id = req.params.id;
+    actionsDb
+      .get(id)
+      .then(action => {
+        res.status(200).json(action);
+      })
+      .catch(error => {
+        res.status(500).json({
+          message: `Error retrieving this particular project: ${error.message}`
+        });
+      });
+  });
 
 module.exports = router;
