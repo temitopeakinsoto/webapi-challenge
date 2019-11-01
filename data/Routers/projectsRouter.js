468b280b71933f575b7aae74f339ca125003e545
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-01 10:53:14 +0100

    Add an endpoint for retrieving projects
    In this commit, I created a new endpoint for retrieving the list of all Projects from the database.

--- data/Routers/projectsRouter.js
+++ data/Routers/projectsRouter.js
@@ -3,5 +3,17 @@ const actionsDb = require("../helpers/actionModel");
 const projectsDb = require("../helpers/projectModel");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  projectsDb
+    .get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: `Error retrieving the list of all Projects: ${error.message}`
+      });
+    });
+});
 
 module.exports = router;
