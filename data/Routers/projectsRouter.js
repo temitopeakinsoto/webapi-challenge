6e0a9690bc1260a69de2e01849fb15d3453db724
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-01 11:16:26 +0100

    Add a post endpoint to projects resource
    In this commit, I added a post route which posts a new project to the project database.
    A validate post middleware was also implemented to check if the new post to be inserted meets the requirements for a new project post

--- data/Routers/projectsRouter.js
+++ data/Routers/projectsRouter.js
@@ -3,6 +3,22 @@ const actionsDb = require("../helpers/actionModel");
 const projectsDb = require("../helpers/projectModel");
 const router = express.Router();
 
+function validateProjectPost(req, res, next){
+    let post = req.body;
+  if (!post || post === "undefined") {
+    res.status(400).json({ message: "missing post data" });
+  } 
+  else if (!post.name) {
+    res.status(400).json({ message: "missing required name field for new post" });
+  } 
+  else if (!post.description){
+    res.status(400).json({ message: "missing required description field for new post" });
+  }
+  else {
+    next();
+  }
+}
+
 router.get("/", (req, res) => {
   projectsDb
     .get()
@@ -15,5 +31,24 @@ router.get("/", (req, res) => {
       });
     });
 });
+
+router.post('/', validateProjectPost, (req, res) => {
+    const { name, description } = req.body;
+    const newPost = {
+        name, description
+    }
+    projectsDb.insert(newPost)
+    .then(post => {
+        res.status(201).json(post)
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: `There was an error creating this project post: ${error}`
+        })
+    })
+
+    res.send(newPost);
+
+})
 
 module.exports = router;
