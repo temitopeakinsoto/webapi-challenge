18e96d20652c50ae959de057e6ece58aa7196f7e
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-01 10:52:08 +0100

    Add an endpoint for retrieving actions
    In this commit, I created a new endpoint for retrieving the list of all actions from the database.

--- data/Routers/actionsRouter.js
+++ data/Routers/actionsRouter.js
@@ -3,5 +3,17 @@ const actionsDb = require("../helpers/actionModel");
 const projectsDb = require("../helpers/projectModel");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  actionsDb
+    .get()
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: `Error retrieving the list of all actions: ${error.message}`
+      });
+    });
+});
 
 module.exports = router;
