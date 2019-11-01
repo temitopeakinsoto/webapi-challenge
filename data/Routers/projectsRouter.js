3d283ecc21e8b58d1f1c5a3fa89f5894675e8d2b
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-01 10:18:00 +0100

    create actionsRouter and projectsRouter for handling CRUD requests to action and projects resources respectively

---
+++ data/Routers/projectsRouter.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const actionsDb = require("../helpers/actionModel");
+const projectsDb = require("../helpers/projectModel");
+const router = express.Router();
+
+
+module.exports = router;
